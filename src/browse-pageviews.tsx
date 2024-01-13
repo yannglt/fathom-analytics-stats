c3ab6a95a6f75bd7c81f22edf907c2ddc6dc76b7
yannglt <helloyannglt@gmail.com>
2024-01-13 16:06:09 +0100

    Update period dropdown component logic with all time functions

--- src/browse-pageviews.tsx
+++ src/browse-pageviews.tsx
@@ -33,71 +33,12 @@ export default function Command() {
     },
   );
 
-  const timePeriods = [
-    { id: "1", title: "Today", value: getCurrentDate() },
-    { id: "2", title: "Yesterday", value: getPreviousDate(1) },
-    { id: "3", title: "Last 7 Days", value: getPreviousDate(7) },
-    { id: "4", title: "Last 30 Days", value: getPreviousDate(30) },
-    { id: "5", title: "Last 365 Days", value: getPreviousDate(365) },
-    { id: "6", title: "This Month", value: getCurrentMonthStartDate() },
-    { id: "7", title: "Last Month", value: getPreviousMonthStartDate() },
-    { id: "8", title: "This Year", value: getCurrentYearStartDate() },
-    { id: "9", title: "Last Year", value: getPreviousYearStartDate() },
-    { id: "10", title: "All Time", value: "" },
-  ];
-
-  function getCurrentDate() {
-    const currentDate = new Date();
-    return formatDate(currentDate);
-  }
-
-  function getPreviousDate(days: number) {
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() - days);
-    return formatDate(currentDate);
-  }
-
-  function getCurrentMonthStartDate() {
-    const currentDate = new Date();
-    const currentMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-    return formatDate(currentMonthStartDate);
-  }
-
-  function getPreviousMonthStartDate() {
-    const currentDate = new Date();
-    const previousMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
-    return formatDate(previousMonthStartDate);
-  }
-
-  function getCurrentYearStartDate() {
-    const currentDate = new Date();
-    const currentYearStartDate = new Date(currentDate.getFullYear(), 0, 1);
-    return currentYearStartDate.getFullYear().toString();
-  }
-
-  function getPreviousYearStartDate() {
-    const currentDate = new Date();
-    const previousYearStartDate = new Date(currentDate.getFullYear() - 1, 0, 1);
-    return formatDate(previousYearStartDate);
-  }
-
-  function formatDate(date: Date) {
-    const year = date.getFullYear();
-    const month = `${date.getMonth() + 1}`.padStart(2, "0");
-    const day = `${date.getDate()}`.padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  }
-
-  const handleTimePeriodChange = (newValue: string) => {
-    setDateFrom(newValue);
-  };
-
   return (
     <List
       isLoading={isLoading}
       navigationTitle="Change time period"
       searchBarPlaceholder="Search your pages"
-      searchBarAccessory={<PeriodDropdown timePeriods={timePeriods} onTimePeriodChange={handleTimePeriodChange} />}
+      searchBarAccessory={<PeriodDropdown setDateFrom={setDateFrom} />}
     >
       {data?.map((page) => (
         <List.Item
